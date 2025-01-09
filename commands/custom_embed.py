f2a8e1062cefae1a846069b6b541cef766b7cbcb
irinque <klenovmatvey@yandex.ru>
2025-01-10 00:09:04 +0300

    variable_names

--- commands/custom_embed.py
+++ commands/custom_embed.py
@@ -1,27 +1,27 @@
 from discord.ui import TextInput, Modal, ChannelSelect, View
 from discord.utils import get
 from discord import Embed, TextStyle, Interaction, Colour, ChannelType, Interaction
-from settings import COLOR_EMBED
+from settings import COLOR_EMBED, label_title_post, label_description_post, label_mention_post, label_image_post, label_title
 
 async def send_custom_embed(interaction: Interaction, role_id):
     async def select_callback(interaction: Interaction):
         channel_name = dropdown.values[0]
         channel = get(interaction.guild.channels, name=str(channel_name))
-        class SendApplication(Modal, title="📝 EMBED"):
-            message_title = TextInput(label="🎴 ЗАГОЛОВОК", style=TextStyle.short)
-            description = TextInput(label="🀄 ОПИСАНИЕ", style=TextStyle.long, required=True)
-            mention = TextInput(label="📣 УПОМИНАНИЕ(Да/Нет)", style=TextStyle.short, required=True)
-            image = TextInput(label="🌄 КАРТИНКА", style=TextStyle.short, required=False)
+        class SendApplication(Modal, title=label_title):
+            title_post = TextInput(label=label_title_post, style=TextStyle.short)
+            description_post = TextInput(label=label_description_post, style=TextStyle.long, required=True)
+            mention_post = TextInput(label=label_mention_post, style=TextStyle.short, required=True)
+            image_post = TextInput(label=label_image_post, style=TextStyle.short, required=False)
             async def on_submit(self, interaction: Interaction):
-                embed = Embed(title=self.message_title, description=self.description, colour=Colour.from_str(COLOR_EMBED))
-                if self.image:
-                    embed.set_image(url=self.image)
+                embed = Embed(title=self.title_post, description=self.description_post, colour=Colour.from_str(COLOR_EMBED))
+                if self.image_post:
+                    embed.set_image(url=self.image_post)
                 role_user = get(interaction.guild.roles, id=role_id)
-                if str(self.mention).lower() == "да":
+                if str(self.mention_post).lower() == "да":
                     await channel.send(content=f"{role_user.mention}", embed=embed)
                     await interaction.response.send_message(f"✅")
                     await interaction.delete_original_response()
-                if str(self.mention).lower() == "нет":
+                if str(self.mention_post).lower() == "нет":
                     await channel.send(embed=embed)
                     await interaction.response.send_message(f"✅")
                     await interaction.delete_original_response()
